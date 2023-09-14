21739fb38601f7ccbe56454086dde3c5901e3543
Wissarut Kanasub <wissarut.dev@gmail.com>
2023-09-14 19:45:36 +0700

    Add sign up page

--- mysite/urls.py
+++ mysite/urls.py
@@ -18,9 +18,12 @@ from django.contrib import admin
 from django.urls import include, path
 from django.views.generic import RedirectView
 
+from mysite import views
+
 urlpatterns = [
     path('', RedirectView.as_view(url='polls/', permanent=False)),
     path('polls/', include('polls.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
+    path('signup/', views.signup, name='signup'),
     path('admin/', admin.site.urls),
 ]
